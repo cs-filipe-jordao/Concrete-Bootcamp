df792ac9a5cf1f9721306c4dddfaa30dfb5aa0b6
Filipe Nogueira Jordão <filipe.n.jordao@concrete.com.br>
2019-04-29 16:49:40 -0300

    Add base project

---
+++ Magic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic
+//
+//  Created by filipe.n.jordao on 29/04/19.
+//  Copyright © 2019 filipe.n.jordao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
