8a96aa370ab4a318e456ae06f786d2dfc6026cf7
Filipe Nogueira Jordão <filipe.n.jordao@concrete.com.br>
2019-04-30 09:05:37 -0300

    Add xcodegen config

---
+++ MagicAPI/Models/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  MagicAPI
+//
+//  Created by filipe.n.jordao on 30/04/19.
+//  Copyright © 2019 filipe.n.jordao. All rights reserved.
+//
+
+import Foundation
+
+public struct Card: Codable {
+    
+}
---
+++ MagicAPI/Models/CardSet.swift
@@ -0,0 +1,13 @@
+//
+//  CardSet.swift
+//  MagicAPI
+//
+//  Created by filipe.n.jordao on 30/04/19.
+//  Copyright © 2019 filipe.n.jordao. All rights reserved.
+//
+
+import Foundation
+
+public struct CardSet: Codable {
+
+}
