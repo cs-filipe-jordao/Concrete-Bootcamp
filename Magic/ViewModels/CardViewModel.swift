1adf9aa4bbf3635034212294c63d04c259ec6e8e
Filipe Nogueira Jordão <filipe.n.jordao@concrete.com.br>
2019-05-06 13:59:41 -0300

    Add CardViewModel

---
+++ Magic/ViewModels/CardViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  CardViewModel.swift
+//  Magic
+//
+//  Created by filipe.n.jordao on 06/05/19.
+//
+
+import Foundation
+
+class CardViewModel {
+    let imageURL: URL?
+
+    init(imageURL: URL?) {
+        self.imageURL = imageURL
+    }
+}
