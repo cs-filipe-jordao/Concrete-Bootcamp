72002885be3e963ffbeb09700f4741c0c4c3be5a
Filipe Nogueira Jordão <filipe.n.jordao@concrete.com.br>
2019-05-07 10:39:38 -0300

    Add CardCell view

---
+++ Magic/UI/CardCell.swift
@@ -0,0 +1,42 @@
+//
+//  CardCell.swift
+//  Magic
+//
+//  Created by filipe.n.jordao on 07/05/19.
+//
+
+import UIKit
+import SnapKit
+
+class CardCell: UICollectionViewCell {
+    let banner = UIImageView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CardCell: CodeView {
+    func setupViews() {
+        banner.contentMode = .scaleAspectFill
+    }
+
+    func setupHierarchy() {
+        contentView.addSubview(banner)
+    }
+
+    func setupConstraints() {
+        banner.snp.makeConstraints { make in
+            make.left
+                .top
+                .bottom
+                .right
+                .equalTo(contentView)
+        }
+    }
+}
---
+++ Magic/UI/CodeView.swift
@@ -0,0 +1,22 @@
+//
+//  CodeView.swift
+//  Magic
+//
+//  Created by filipe.n.jordao on 07/05/19.
+//
+
+import Foundation
+
+protocol CodeView {
+    func setupViews()
+    func setupHierarchy()
+    func setupConstraints()
+}
+
+extension CodeView {
+    func configureView() {
+        setupViews()
+        setupHierarchy()
+        setupConstraints()
+    }
+}
