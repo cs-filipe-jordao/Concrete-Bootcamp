7f2589180673226a144a2a86efccb6e5878d4a9a
Filipe Nogueira Jordão <filipe.n.jordao@concrete.com.br>
2019-05-07 13:30:26 -0300

    Add CardSetListView

---
+++ Magic/CardSetList/UI/CardSetListView.swift
@@ -0,0 +1,62 @@
+//
+//  CardSetListView.swift
+//  Magic
+//
+//  Created by filipe.n.jordao on 07/05/19.
+//  Copyright © 2019 filipe.n.jordao. All rights reserved.
+//
+
+import UIKit
+
+final class CardSetListView: UIView {
+    let background = UIImageView()
+
+    let collection: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        flowLayout.minimumLineSpacing = 15
+
+        return UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CardSetListView: CodeView {
+    func setupViews() {
+        background.image = #imageLiteral(resourceName: "Background")
+        
+        collection.backgroundView = nil
+        collection.backgroundColor = .clear
+    }
+
+    func setupHierarchy() {
+        [ background,
+          collection ].forEach(self.addSubview)
+    }
+
+    func setupConstraints() {
+        background.snp.makeConstraints { make in
+            make.top
+                .bottom
+                .left
+                .right
+                .equalTo(self)
+        }
+
+        collection.snp.makeConstraints { make in
+            make.top
+                .bottom
+                .left
+                .right
+                .equalTo(self)
+        }
+    }
+}
---
+++ MagicTests/CardSetList/UI/CardSetListViewSpec.swift
@@ -0,0 +1,25 @@
+//
+//  CardSetListViewSpec.swift
+//  MagicTests
+//
+//  Created by filipe.n.jordao on 07/05/19.
+//
+
+import Quick
+import Nimble
+import Nimble_Snapshots
+
+@testable import Magic
+
+class CardSetListViewSpec: QuickSpec {
+    override func spec() {
+        describe("A CardSetListView") {
+            context("When its created") {
+                let sut = CardSetListView(frame: CGRect(x: 0, y: 0, width: 400, height: 800))
+                it("Should render as expected") {
+                    expect(sut) == recordSnapshot("CardSetListView")
+                }
+            }
+        }
+    }
+}
